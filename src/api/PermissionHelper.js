828f09d6048e291b5d9a5a0dce5792e5ce720d61
aizhi.cao <aizhicao.88@126.com>
2019-11-14 10:44:34 +0800

    router filter and get role class of job management

---
+++ src/api/PermissionHelper.js
@@ -0,0 +1,105 @@
+import {Uint64BE} from "int64-buffer";
+
+export default class PermissionHelper {
+  /**
+   * index(0) => ID(1): Overview
+   * index(1) => ID(2): Inspection
+   * index(2) => ID(4): Event
+   * index(3) => ID(8): Statistics
+   * index(4) => ID(16): Settings
+   */
+  static data = [];
+
+  static setData(data){
+    (data.length > 0)&&(this.data = data);
+  }
+
+  static enableAuthorities(index,bigEndian,littleEndian){
+    if(this.data.length == 0){
+      return true;
+    }
+
+    let authority = new Uint64BE(this.data[index].toString()).toString(10);
+    let base = new Uint64BE(bigEndian,littleEndian).toString(10);
+
+    return authority&base;
+  }
+  // index(0) => ID(1): Overview
+  static enableRemoteOverview(){
+    return this.enableAuthorities(0,0x0,0x1);
+  }
+  static enableEventOverview(){
+    return this.enableAuthorities(0,0x0,0x2);
+  }
+
+  // index(1) => ID(2): Inspection
+  static enableRemoteInspect(){
+    return this.enableAuthorities(1,0x0,0x1);
+  }
+  static enableLocalInspect(){
+    return this.enableAuthorities(1,0x0,0x2);
+  }
+
+  static enableInspectReport(){
+    return this.enableAuthorities(1,0x0,0x4);
+  }
+  static enablePatrolTask(){
+    return this.enableAuthorities(1,0x0,0x8);
+  }
+  static enableStoreMonitor(){
+    return this.enableAuthorities(1,0x0,0x10);
+  }
+  static enableTransactionPatrol(){
+    return this.enableAuthorities(1,0x0,0x20);
+  }
+  static enableStorePointCheck(){
+    return this.enableAuthorities(1,0x0,0x40);
+  }
+  //index(2) => ID(4): Event
+  static enableEventHandle(){
+    return this.enableAuthorities(2,0x0,0x1);
+  }
+  static enableEventClose(){
+    return this.enableAuthorities(2,0x0,0x2);
+  }
+  static enableEventAdd(){
+    return this.enableAuthorities(2,0x0,0x4);
+  }
+
+  // index(3) => ID(8): Statistics
+  static enablePatrolEvaStatistics(){
+    return this.enableAuthorities(3, 0x0,0x1);
+  }
+  static enableInspectStatistics(){
+    return this.enableAuthorities(3, 0x0,0x2);
+  }
+  static enableEventStatistics(){
+    return this.enableAuthorities(3, 0x0,0x4);
+  }
+  static enableSupervisionEffStatistics(){
+    return this.enableAuthorities(3, 0x0,0x8);
+  }
+  static enableStatistics(){
+    return this.enableAuthorities(3, 0x8,0x0);
+  }
+  //index(4) => ID(16): Settings
+  static enablePatrolSetting(){
+    return this.enableAuthorities(4, 0x0,0x1);
+  }
+  static enableDeviceSetting(){
+    return this.enableAuthorities(4, 0x0,0x2);
+  }
+  static enableStoreSetting(){
+    return this.enableAuthorities(4, 0x0,0x4);
+  }
+  static enableScheduleSetting(){
+    return this.enableAuthorities(4, 0x0,0x8);
+  }
+  static enableTitleSetting(){
+    return this.enableAuthorities(4, 0x0,0x10);
+  }
+  static getInterget(){
+    let big = new Uint64BE(0x1, 0x1);
+
+  }
+}
