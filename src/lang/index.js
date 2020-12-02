b719526dd8ffa3dc79ac72f31e57e9f48876eb10
aizhi.cao <aizhicao.88@126.com>
2020-12-02 11:11:07 +0800

    optimize multi-language based on review advice

--- src/lang/index.js
+++ src/lang/index.js
@@ -1,15 +1,14 @@
-import Vue from 'vue'
-import VueI18n from 'vue-i18n'
-import Cookies from 'js-cookie'
-import elementEnLocale from 'element-ui/lib/locale/lang/en' // element-ui lang
-import elementZhLocale from 'element-ui/lib/locale/lang/zh-CN'// element-ui lang
-import elementZhTWLocale from 'element-ui/lib/locale/lang/zh-TW'// element-ui lang
-import enLocale from './en'
-import zhcnLocale from './zhcn'
-import zhtwLocale from './zhtw'
-import {getCookie} from "../common/auth";
+import Vue from 'vue';
+import VueI18n from 'vue-i18n';
+import elementEnLocale from 'element-ui/lib/locale/lang/en'; // element-ui lang
+import elementZhLocale from 'element-ui/lib/locale/lang/zh-CN';// element-ui lang
+import elementZhTWLocale from 'element-ui/lib/locale/lang/zh-TW';// element-ui lang
+import enLocale from './en';
+import zhcnLocale from './zhcn';
+import zhtwLocale from './zhtw';
+import { getCookie } from '../common/auth';
 
-Vue.use(VueI18n)
+Vue.use(VueI18n);
 
 const messages = {
   en: {
@@ -24,11 +23,11 @@ const messages = {
     ...zhtwLocale,
     ...elementZhTWLocale
   }
-}
+};
 
 const i18n = new VueI18n({
   locale: getCookie('lang') || 'en', // set locale
   messages // set locale messages
-})
+});
 
-export default i18n
+export default i18n;
