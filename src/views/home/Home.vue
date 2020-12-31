ac50e27f42093114d566f5fbab8ee46084deb604
aizhi.cao <aizhicao.88@126.com>
2020-12-31 12:59:02 +0800

    update version

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -174,7 +174,7 @@
           </el-col>
           <el-col :sapn="24" class="footercontent">
             <footer class="footerInfo">
-              <p style="text-align:left;">v1.5.2 &copy; {{ getFullYear }} Advantech Intelligent City Services Co., Ltd. (AiCS) All Rights Reserved.</p>
+              <p style="text-align:left;">v1.6.0 &copy; {{ getFullYear }} Advantech Intelligent City Services Co., Ltd. (AiCS) All Rights Reserved.</p>
             </footer>
           </el-col>
 
