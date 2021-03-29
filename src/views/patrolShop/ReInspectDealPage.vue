2688b31cd3c55d7a5055f229f9a27197621c7f1f
aizhi.cao <aizhicao.88@126.com>
2021-03-29 10:17:37 +0800

    cancel to send remote inspection result to leader

--- src/views/patrolShop/ReInspectDealPage.vue
+++ src/views/patrolShop/ReInspectDealPage.vue
@@ -3,45 +3,8 @@
     <div class="page-icon">
       <div class="icon-content">
         <img :src="isSuccess?sucSrc:errSrc" class="suc-icon">
-        <p :class="isSuccess?'sucInfo':'errInfo'">{{ retInfo }}</p>
         <p v-if="isSuccess && lang !== 'en'" class="sucret-info">{{ curSecond }}s{{ $t('remotePatrol.return') }}</p>
         <p v-if="isSuccess && lang === 'en'" class="sucret-info">{{ $t('remotePatrol.return') }} {{ curSecond }}s !</p>
-      </div>
-    </div>
-    <div v-if="false" :style="{'min-height':varyWindowHeight-460+'px'}" class="page-content">
-      <div class="details">
-        <span class="event-label">门店名称：</span>
-        <span>{{ storeName }}</span>
-      </div>
-      <div v-if="tableData.length!=0" class="table-content">
-        <div class="table-header">
-          <div v-for="(item,index) in headerList" :key="index" :style="{'width':item.width+'%'}" class="table-details">
-            <span>{{ item.label }}</span>
-          </div>
-        </div>
-        <div v-for="(item,index) in tableData" :key="index" :style="index%2!=0?{'background-color':'#F4F5F9'}:{'background-color':'#fff'}" class="data-rows">
-          <div class="item-cols" style="width:20%;">
-            <span>{{ item.groupName }}</span>
-          </div>
-          <div class="item-cols" style="width:20%;">
-            <span :style="item.result==0?{'background-color':'#FDBA40'}:{'background-color':'#6097F3'}" class="icon-span">{{ item.result==0?'不合格':'合格' }}</span>
-          </div>
-          <div class="item-cols" style="width:20%;">
-            <span>{{ item.score }}</span>
-          </div>
-          <div class="item-cols" style="">
-            <span>{{ item.totalScore }}</span>
-          </div>
-        </div>
-      </div>
-      <div class="details">
-        <span class="event-label">本次忽略{{ ignoreCount }}个问题</span>
-        <div class="ignore-content">
-          <div v-for="(item,index) in ignoreList" :key="index" class="ignore-details">
-            <div class="icon-point"/>
-            <span class="ignore-info">{{ item.subject }}</span>
-          </div>
-        </div>
       </div>
     </div>
     <div v-if="!isSuccess" class="page-err-btn">
@@ -61,57 +24,17 @@ export default {
       sucSrc: require('../../../static/img/submit_succeed.png'),
       errSrc: require('../../../static/img/submit_failed.png'),
       curSecond: 10,
-      leader: '',
-      storeName: '',
-      ignoreCount: 0,
-      headerList: [
-        {
-          'name': 'groupName',
-          'label': '项目',
-          'width': 20
-        },
-        {
-          'name': 'result',
-          'label': '是否合格',
-          'width': 20
-        },
-        {
-          'name': 'score',
-          'label': '得分',
-          'width': 20
-        },
-        {
-          'name': 'totalScore',
-          'label': '单项总分值',
-          'width': 40
-        }
-      ],
-      tableData: [],
-      ignoreList: [],
       timeid: 0,
-      inspectItemList: [],
-      varyWindowHeight: window.innerHeight,
       lang: this.$i18n.locale
     };
   },
-  computed: {
-    retInfo() {
-      let msg = '';
-      if (this.isSuccess) {
-        if (this.leader != undefined && this.leader.length != 0) {
-          msg = `${this.$t('remotePatrol.copyFail')}${this.leader}！`;
-        }
-      } else {
-        msg = `${this.$t('remotePatrol.sentFail')}`;
-      }
-      return msg;
-    }
-  },
+
   beforeRouteLeave(to, from, next) {
     const self = this;
     clearInterval(self.timeid);
     next();
   },
+
   mounted() {
     const self = this;
     self.getRouterData();
@@ -121,12 +44,12 @@ export default {
       }, 1000);
     }
   },
+
   methods: {
     getRouterData() {
       const self = this;
       let routeData = self.$route.params.data;
-      console.log(routeData);
-      if (self.$route.params.data == undefined) {
+      if (self.$route.params.data === undefined) {
         routeData = JSON.parse(sessionStorage.getItem('reinspect_submit'));
       }
       if (routeData != null) {
@@ -134,12 +57,9 @@ export default {
       }
       if (self.isSuccess) {
         self.$route.matched[2].name = 'successSubmit';
-        if (routeData != null && routeData.user.length != 0) {
-          self.leader = routeData.user[0].userName;
-        }
       } else {
         self.$route.matched[2].name = 'failSubmit';
-        if (self.isSuccess == false) {
+        if (!self.isSuccess) {
           PubSub.publish('success-page', { changeStyle: true });
         }
       }
@@ -148,7 +68,7 @@ export default {
     getBackSecond() {
       const self = this;
       self.curSecond--;
-      if (self.curSecond == 0) {
+      if (self.curSecond === 0) {
         clearInterval(self.timeid);
         self.$router.push({ name: 'remotePatrol' });
       }
@@ -156,7 +76,7 @@ export default {
 
     reTry() {
       const self = this;
-      self.$router.push({ name: 'confirmSum',params: self.$route.params.data.reLoadData});
+      self.$router.push({ name: 'confirmSum', params: self.$route.params.data.reLoadData });
     }
   }
 };
