251cdc675f30f8d86f9be1e6b666ee424c7af755
aizhi.cao <aizhicao.88@126.com>
2021-04-02 09:45:39 +0800

    only view events details when no checked roles

--- src/views/event/details/RateManage.vue
+++ src/views/event/details/RateManage.vue
@@ -1001,6 +1001,7 @@ export default {
         isActive: false
       });
       self.subBtnList = tempBtnList;
+      self.showWinpBtn = tempBtnList.length !== 0;
     },
 
     showRelatedChannel() {
