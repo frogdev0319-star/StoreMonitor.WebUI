e162194deca13df101b96bfcf53502a18dfe3f72
aizhi.cao <aizhicao.88@126.com>
2021-04-02 15:28:23 +0800

    package the elementUI component

---
+++ src/components/DelayButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-button
+    v-bind="$attrs"
+    :loading="loadingStatus"
+    class="storevue-button"
+    @click="handleClick"
+  >
+    <slot/>
+  </el-button>
+</template>
+
+<script>
+export default {
+  name: 'DelayButton',
+  props: {
+    autoLoading: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      loadingStatus: false,
+      lang: this.$i18n.locale
+    };
+  },
+  methods: {
+    handleClick() {
+      if (this.autoLoading) {
+        this.loadingStatus = true;
+      }
+      this.$emit('click', () => {
+        this.loadingStatus = false;
+      });
+    }
+  }
+};
+</script>
+
---
+++ src/components/DialogPop.vue
@@ -0,0 +1,70 @@
+<template>
+  <el-dialog
+    v-bind="$attrs"
+    :visible.sync="dialogVisible"
+    :width="dialogWidth"
+    :height="dialogHeight"
+    class="storevue-dialog"
+  >
+    <div class="dialog-body">
+      <slot/>
+    </div>
+    <div v-if="showButton" slot="footer">
+      <el-button class="cancel-btn" size="mini" @click="cancelHandler">
+        {{ $t('remotePatrol.cancel') }}
+      </el-button>
+      <el-button class="confirm-btn" size="mini" type="primary" @click="confirmHandler">
+        {{ $t('remotePatrol.confirm') }}
+      </el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  name: 'DialogPop',
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    },
+    dialogWidth: {
+      type: String,
+      default: '500px'
+    },
+    dialogHeight: {
+      type: String,
+      default: '240px'
+    },
+    showButton: {
+      type: Boolean,
+      default: true
+    }
+  },
+
+  computed: {
+    dialogVisible: {
+      get() {
+        return this.visible;
+      },
+      set(val) {
+        this.$emit('visibleChangeHandler', val);
+      }
+    }
+  },
+
+  methods: {
+    cancelHandler() {
+      this.$emit('cancelHandler');
+    },
+
+    confirmHandler() {
+      this.$emit('confirmHandler');
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
