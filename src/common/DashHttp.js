c76a4e3ae1878ede699d4421b27a836dbc3411af
aizhi.cao <aizhicao.88@126.com>
2021-04-09 10:17:48 +0800

    delete unused code

--- src/common/DashHttp.js
+++ src/common/DashHttp.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { getDashServerInfo } from '@/api/device.js';
-import store from '../store/index.js';
 import { message } from '@/common/singleton-message';
 
 axios.defaults.withCredentials = false;
