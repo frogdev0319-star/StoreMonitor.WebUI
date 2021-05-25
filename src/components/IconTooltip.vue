f4ea7ada15e2fd2490a1b9afbdf1b55f4b6553f8
aizhi.cao <aizhicao.88@126.com>
2021-05-25 13:55:51 +0800

    package el-tooltip component

---
+++ src/components/IconTooltip.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-tooltip class="item" v-bind="$attrs">
+    <div slot="content" v-if="isCatergySetting">
+      <span>{{ $t('insSettingView.parentCatergyNameTooltip1')}} <br/> {{ $t('insSettingView.parentCatergyNameTooltip2')}}</span>
+    </div>
+    <slot></slot>
+  </el-tooltip>
+</template>
+
+<script>
+    export default {
+      name: "IconTooltip",
+      props:{
+        tooltipContent: {
+          type: String,
+          default: ''
+        },
+        isCatergySetting: {
+          type: Boolean,
+          default: false
+        }
+      },
+      data(){
+          return {
+            newLine:'<br>'
+          }
+      },
+    }
+</script>
+
+<style scoped>
+
+</style>
