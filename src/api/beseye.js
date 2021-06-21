1d7f0c6f7e61dac7e2785819b1868a7a35b60699
aizhi.cao <aizhicao.88@126.com>
2021-06-21 14:02:28 +0800

    merge - beseye video component

---
+++ src/api/beseye.js
@@ -0,0 +1,114 @@
+import request from '@/common/request';
+import Environment from '@/common/environment';
+import * as axios from 'axios';
+import { getToken } from '@/common/auth.js';
+
+const version = Environment.VERSION;
+
+const VCAMID = '93aed0a82f284c9884c1956121db809f';
+const DD = '{Web}_{YH-EXTERNAL}_{97557807-1149-4004-8e92-91c344539ded}';
+
+const baseUrl = Environment.BASE_URL;
+
+export const getStreamInfo = params => {
+  return request({
+    url: `${version}/beseye/stream/info`,
+    method: 'get',
+    params
+  });
+};
+
+export const getBeseyeAccessToken = params => {
+  return request({
+    url: `${version}/beseye/token`,
+    method: 'get',
+    params
+  });
+};
+
+const instance = axios.create({
+  baseURL: 'http://104.199.172.143/api/',
+  timeout: 10000,
+  headers: {
+    'Accept': 'application/json'
+  }
+});
+
+export const getStreamInfoFromTW = params => {
+  return instance({
+    url: `beseye/streamInfo`,
+    method: 'get',
+    params
+  });
+};
+
+export const getBeseyeDeviceList = data => {
+  return request({
+    url: `${version}/beseye/device/list`,
+    method: 'post',
+    data
+  });
+};
+
+export const addBeseyeDevice = data => {
+  return request({
+    url: `${version}/beseye/device/add`,
+    method: 'post',
+    data
+  });
+};
+export const updateBeseyeDevice = data => {
+  return request({
+    url: `${version}/beseye/device/update`,
+    method: 'post',
+    data
+  });
+};
+
+export const deleteBeseyeDevice = data => {
+  return request({
+    url: `${version}/beseye/device/delete`,
+    method: 'post',
+    data
+  });
+};
+
+export const getPlaylistInfo = params => {
+  return request({
+    url: `${version}/beseye/playlist`,
+    method: 'get',
+    params
+  });
+};
+
+export const getBeseyeUserList = params => {
+  return request({
+    url: `${version}/beseye/account/list`,
+    method: 'post',
+    params
+  })
+}
+
+export const deleteBeseyeUser = data => {
+  return request({
+    url: `${version}/beseye/account/delete`,
+    method: 'post',
+    data
+  });
+}
+
+export const beseyeAccountAuthorize = data => {
+  return request({
+    url: `${version}/beseye/account/authorize`,
+    method: 'post',
+    data
+  })
+}
+
+export const getAvailableBeseyeDevice = params => {
+  return request({
+    url: `${version}/beseye/device/unused/list`,
+    method: 'get',
+    params
+  })
+}
---
+++ src/common/BeseyeAuthorizeConfig.js
@@ -0,0 +1,13 @@
+const BeseyeAuthorizeConfig = {
+  baseOAuthUrl: 'https://oregon-p1-stage-api-1.beseye.com/',
+  userAuthorizationUri: 'https://oregon-p1-stage-api-1.beseye.com/oauth/oauth/authorize',
+  redirect_uri: 'https://preview-inspect.storevue.com/storemonitor_ui/beseye/authorize',
+  client_id: 'c96573eb5b721ebd133963de5007ee5c9c86c733e288710a61db2b574683e94a',
+  scope: 'storevue',
+  state: 'a00caec8dbd08e50',
+  response_type: 'code',
+  code: '',
+  vcamerId: '3ecf3a6ca0884462ac5c090c25c0ee27'
+};
+
+export default BeseyeAuthorizeConfig;
