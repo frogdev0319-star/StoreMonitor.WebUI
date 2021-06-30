b5801128906edf9084226bcf25a280b5a17b90ee
aizhi.cao <aizhicao.88@126.com>
2021-06-30 11:04:06 +0800

    modify - modify code

--- src/views/setting/report/ReportSetting.vue
+++ src/views/setting/report/ReportSetting.vue
@@ -228,7 +228,7 @@ export default {
       const switches = this.curTemplate.config.switches;
       util.sortArrayByKeyAsc(switches, 'position');
       this.basicList = switches.slice(1, 4);
-      this.list = switches.slice(4, switches.length - 1);
+      this.list = switches.slice(4);
       this.ifShowAllDetails = switches[0].enable ? 1 : 0;
     },
 
@@ -418,11 +418,6 @@ export default {
           'enable': !!this.ifShowAllDetails,
           'name': 'defaultAll',
           'position': -1
-        },
-        {
-          'enable': false,
-          'name': 'notJoinItem',
-          'position': 8
         }
       );
       const updateTemplParmas = {};
