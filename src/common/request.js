6e8b74419f6aab0f9e89442ce879e5baef0d3d4d
aizhi.cao <aizhicao.88@126.com>
2021-07-14 13:41:28 +0800

    modify - modify code

--- src/common/request.js
+++ src/common/request.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { Message, MessageBox } from 'element-ui';
+import { MessageBox } from 'element-ui';
 import store from '@/store';
 import router from '@/router';
 import i18n from '@/lang';
@@ -8,9 +8,6 @@ import Environment from './environment.js';
 import { message } from '@/common/singleton-message';
 
 const baseUrl = Environment.BASE_URL;
-// create an axios instance
-
-const itempath = '/storemonitor/api/';
 
 axios.defaults.withCredentials = true;
 const service = axios.create({
@@ -126,7 +123,6 @@ service.interceptors.request.use(
 
 service.interceptors.response.use(
   response => {
-    console.log(response)
     return response.data;
   }, err => {
     if (err.code === 'ECONNABORTED' || err.message === 'Network Error') {
