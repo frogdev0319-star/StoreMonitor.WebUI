01030a6f95cab8c0eb8b00bc479495ffd4256c32
aizhi.cao <aizhicao.88@126.com>
2021-07-27 16:06:35 +0800

    modify - modify element style file and import el-image style

--- src/main.js
+++ src/main.js
@@ -9,7 +9,6 @@ import 'vue-video-player/src/custom-theme.css';
 import 'videojs-flash';
 import router from './router';
 import axios from 'axios';
-import { getToken } from '@/common/auth';
 import { message } from '@/common/singleton-message';
 import Print from '@/plugins/print';
 import { ProgressPlugin } from 'bootstrap-vue';
