6febe0c434ada3a153dc95659314da4bbbbe0e27
aizhi.cao <aizhicao.88@126.com>
2021-08-05 10:43:52 +0800

    feature - add checkin staticstical position

--- src/views/setting/title/TitleSetting.vue
+++ src/views/setting/title/TitleSetting.vue
@@ -236,6 +236,11 @@ export default {
               roleName: this.$t('route.supervisorStat'),
               checked: false,
               disabled: false
+            },
+            {
+              roleName: this.$t('route.checkInStatistics'),
+              checked: false,
+              disabled: false
             }
           ]
         },
@@ -352,6 +357,7 @@ export default {
       this.roleNameList[3].children[1].checked = !!PermissionHelper.enableInspectStatistics();
       this.roleNameList[3].children[2].checked = !!PermissionHelper.enableEventStatistics();
       this.roleNameList[3].children[3].checked = !!PermissionHelper.enableSupervisionEffStatistics();
+      this.roleNameList[3].children[4].checked = !!PermissionHelper.enableCheckinStatistics();
 
       this.roleNameList[4].children[0].checked = !!PermissionHelper.enableDeviceSetting();
       this.roleNameList[4].children[1].checked = !!PermissionHelper.enablePatrolSetting();
