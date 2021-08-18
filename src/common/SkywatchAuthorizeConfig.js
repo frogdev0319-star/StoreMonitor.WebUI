3deba415c49b6a9656a6d6a562ae7503f2d0ec23
aizhi.cao <aizhicao.88@126.com>
2021-08-18 15:40:15 +0800

    feature - get redirect url based on production environment

--- src/common/SkywatchAuthorizeConfig.js
+++ src/common/SkywatchAuthorizeConfig.js
@@ -1,7 +1,16 @@
+import Environment from './environment.js';
+
 
 const SkywatchAuthorizeConfig = {
   userAuthorizationUri: 'https://service.skywatch24.com/oauth2',
-  redirect_server_uri: 'http://172.21.81.63:8081/storemonitor_ui/skywatch',
+  redirect_server_uri: getRedirectUrl(),
 };
+
+function getRedirectUrl() {
+  const baseUrl = Environment.BASE_URL;
+  const storemonitorIndex = baseUrl.indexOf('storemonitor');
+  const redirectUrl = `${baseUrl.substr(0, storemonitorIndex)}storemonitor_ui/skywatch`;
+  return redirectUrl;
+}
 
 export default SkywatchAuthorizeConfig;
