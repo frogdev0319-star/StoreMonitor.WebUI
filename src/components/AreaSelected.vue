390291062ae58b955488afc6b0ec2c0459900626
Rainney <rainney.chen@advantech.com.tw>
2021-12-24 15:00:37 +0800

    1. AreaSelected 全選id是-1問題 2.export excel

--- src/components/AreaSelected.vue
+++ src/components/AreaSelected.vue
@@ -398,6 +398,7 @@ export default {
         storeId=this.doGetStoreIdsByCity(selectedArray);
         //console.log("storeId:",storeId);
       }
+      
       this.$emit("emitTypeChanged",{compareType:this.compareType,compareArr:storeId,selectedLabels});
       //this.changeStoreNew(arr);
     },
