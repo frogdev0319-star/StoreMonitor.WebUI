b83ea4044fd993e65e54e8ce8090524f8601db10
Rainney <rainney.chen@advantech.com.tw>
2022-01-20 17:42:11 +0800

    add 巡檢表選項 box-shadow

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -461,6 +461,8 @@ export default {
         width:calc(222/1440*100vw);
         height: 36px;
         background-color: #fff;
+        border-radius: 5px;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.06);
       }
       .el-province{
         width: calc(222/1440*100vw);
