761399546ad0f05212f945bacffdb6ed195f627e
Rainney <rainney.chen@advantech.com.tw>
2022-02-25 13:01:06 +0800

    FIX:沒有設立達標分的巡檢表仍會出現達標狀態

--- src/views/patrolShop/InspectReportList.vue
+++ src/views/patrolShop/InspectReportList.vue
@@ -146,7 +146,7 @@
                         2: {'color':'#59ab22','background-color':'#e8f6de'}
                       }[item.statusCode]"
                     >{{item.status}}</div>
-                    <div style="margin-left: calc(10/1920*100vw)" class="status-tag"
+                    <div v-if="item.standard!=0" style="margin-left: calc(10/1920*100vw)" class="status-tag"
                       :style="item.standard==1 ? {'color':'#59ab22','background-color':'#e8f6de'}: {'color':'#f57848','background-color':'#ffefeb'}"
                     >{{item.standard==1 ? $t('remotePatrol.goalAchieved') : $t('remotePatrol.farBehind')}}</div>
                   </div>
