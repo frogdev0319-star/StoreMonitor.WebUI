dea2128631f08a0e66518355fb3484ced2601cc5
Rainney <rainney.chen@advantech.com.tw>
2022-05-06 17:32:33 +0800

    update version 3.0.2.19

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -248,7 +248,7 @@
           <el-col :sapn="24" class="footercontent">
             <footer class="footerInfo">
               <p style="text-align: left">
-                v3.0.2.18
+                v3.0.2.19
                  &copy; {{ getFullYear }} Advantech Intelligent City
                 Services Co., Ltd. (AiCS) All Rights Reserved.
               </p>
