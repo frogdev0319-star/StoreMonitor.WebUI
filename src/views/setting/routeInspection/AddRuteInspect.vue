386ee52d3d2659fe2fb7d17e432fe9086ce0a0a2
ron.ho <ron.ho@advantech.com.tw>
2022-05-17 11:31:52 +0800

    權重設定

--- src/views/setting/routeInspection/AddRuteInspect.vue
+++ src/views/setting/routeInspection/AddRuteInspect.vue
@@ -89,8 +89,9 @@
                         <div class="group-left spacer">
                           <div v-if="activeParentId === item.id && !item.children" class="proper-flag"/>
                           <div :style="activeParentId === item.id?{'color':'#006ab7'}:{}" class="flex" style="flex-direction: column; height: 60px; text-align: left; padding-left: 20px;">
-                            <div class="spacer" :style="activeSheetName != 2 ? {}: {'visibility': 'hidden'}" style="color: #c60957; line-height: 30px">{{ item.weight == -1 ? '--' : `${item.weight} %` }}</div> 
-                            <div class="spacer" style="line-height: 30px">{{ item.name }}（{{ item.groupNum }}）</div>
+                            <div v-if="item.weight != -1" :style="activeSheetName != 2 ? {}: {'visibility': 'hidden'}" style="line-height: 30px">{{ `${item.weight} %` }}</div> 
+                            <div class="spacer"></div>
+                            <div :style="item.weight != -1 ? {'line-height': '30px', 'height': '30px'} : {'line-height': '60px', 'height': '60px'}">{{ item.name }}（{{ item.groupNum }}）</div>
                           </div>
                         </div>
                         <div class="spacer"></div>
