f91d405a21e3fe21695c23c76e38fcb792f75bc1
Rainney <rainney.chen@advantech.com.tw>
2022-08-03 17:07:24 +0800

    移除Debug log

--- src/views/setting/routeInspection/RouteInspection.vue
+++ src/views/setting/routeInspection/RouteInspection.vue
@@ -1303,7 +1303,6 @@ export default {
         const item = {};
         rowCells.forEach(cell => {
           const key = cell.header && cell.header.tag;
-          console.log("mapping[key]:",mapping[key])
           if (!mapping[key]) return;
           if (mapping[key] === 'availableScores') {
             item[mapping[key]] = cell.v ? cell.v.split('/').map(item => Number(item)) : cell.v === 0 ? [0] : [];
@@ -1316,8 +1315,6 @@ export default {
           } else if (mapping[key] === 'description') {
             item[mapping[key]] = cell.v ? cell.v.toString().substring(0, 1200) : '';
           } else if (mapping[key] === 'required') {
-            console.log("mapping[key] === 'required'")
-            console.log("cell.v:",cell.v)
             item[mapping[key]] = cell.v === 'Y' || cell.v === 'y'
           } else {
             item[mapping[key]] = cell.v || '';
@@ -1757,13 +1754,13 @@ export default {
           let ScoreCopy = deepClone(Score);
           let OthersCopy = deepClone(Others);
           const tableVersion = _this.getTableVersonBasedOnWeight(PassFail, Score, Others);
-          console.log('**tableVersion:',tableVersion);
+          //console.log('**tableVersion:',tableVersion);
           outdata.PassFail = _this.getPassAndFailSheetJsonData(wb, PassFail, tableVersion);
           outdata.Score = _this.getScoreSheetJsonData(wb, Score, tableVersion);
           outdata.Others = _this.getOthersSheetJsonData(wb, Others, tableVersion);
           
           if (outdata.PassFail.length > 0) {
-            console.log("outdata.PassFail:",outdata.PassFail)
+            //console.log("outdata.PassFail:",outdata.PassFail)
             if (!outdata.PassFail[0].catergyName) {
               PassFailCopy.A2 = {
                 h: _this.$t('insSettingView.Ratingitems'),
@@ -1881,7 +1878,7 @@ export default {
           rowDataObj.itemName = this.getTableCellData(_item.__EMPTY_3);
           rowDataObj.score = _item.__EMPTY_4;
           rowDataObj.description = this.getTableCellData(_item['巡檢項目詳細說明（選填，1200字元）']);
-          console.log('**_item.__EMPTY_5:',_item.__EMPTY_5);
+          //console.log('**_item.__EMPTY_5:',_item.__EMPTY_5);
           rowDataObj.required = (typeof _item.__EMPTY_5=='undefined')?'':_item.__EMPTY_5;
           // if (tableVersion === 1) {
           //   rowDataObj.subCatergyName = '';
@@ -1901,7 +1898,7 @@ export default {
 
           rowDataArray.push(rowDataObj);
         });
-        console.log('**rowDataArray:',rowDataArray);
+        //console.log('**rowDataArray:',rowDataArray);
         return rowDataArray;
       }
       return [];
@@ -2346,7 +2343,7 @@ export default {
           }
         }
       }
-      console.log("**passFailSheet:",passFailSheet);
+      //console.log("**passFailSheet:",passFailSheet);
       return passFailSheet;
     },
 
