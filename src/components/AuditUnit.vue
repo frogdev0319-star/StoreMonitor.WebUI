1b314e6c71c7e8f973771601ac45e705e64939a8
鄧楨樺 <frogdeng0319@gmail.com>
2022-08-07 01:16:58 +0800

    Resolve bugs issue

--- src/components/AuditUnit.vue
+++ src/components/AuditUnit.vue
@@ -1,44 +1,49 @@
 <template>
     <div>
-			
-			<div class="audit-flow-unit" v-for="(taskItem, index) in taskInfo" :key="index" :class="{ not__yet: taskItem.tasks[0].taskId == null }">
-					<div class="check" v-if="taskItem.state == 0"><i class="iconfont el-icon-success iconbangzhu"/></div>
-					<div class="check" v-else-if="taskItem.state == 1"><i class="iconfont el-icon-success iconbangzhu"/></div>
-					<div class="check" v-else-if="taskItem.state == 2"><i class="iconfont el-icon-time iconbangzhu"/></div>
-					<div class="check" v-else-if="taskItem.state == 3"><i class="iconfont el-icon-more iconbangzhu need_grey"/></div>
-					
-					<!-- audit task wrapper -->
-					<div class="audit-task-wrapper" :class="{ on_audit : taskItem.state == 2 }" >
-						<div class="audit-workflow-name">{{taskItem.nodeName}}</div>
-						<div class="audit-flow-content"  v-for=" task in taskItem.tasks" :key="task.taskId">
-							<!-- name -->
-							<div class="for-flex justify-content_space-between" style="margin-bottom: 10px">
-								<div class="audit-name">
-									<div class="audit-user-name" v-if="task.assignee !== null && task.auditByUsers.length == 0 ">{{task.assignee.titleName}} -- {{task.assignee.userName}} <span>({{task.startTs}})</span></div>
-									<div class="audit-user-name" v-else-if="task.assignee == null && task.auditByUsers.length > 0"> {{task.auditByUsers[0].titleName}} -- {{task.auditByUsers[0].userName}} <span>({{taskItem.tasks[0].startTs}})</span></div>
-								</div>
-								<div class="audit-situation"  v-if="taskItem.state == 1">
-									<div class="audit_agree" v-if="task.comment.result == 0 && task.comment.result !== null"><i class="iconfont el-icon-check"/> 同意</div>
-									<div class="audit_disagree" v-else-if="task.comment.result == 1 && task.comment.result !== null"><i class="iconfont el-icon-close"/> 駁回</div>
-									<div class="audit_disagree" v-else-if="task.comment.result == -2"><i class="iconfont el-icon-info"/> 撤回</div>
-								</div>
+		<div class="audit-flow-unit" v-for="(taskItem, index) in taskInfo" :key="index" :class="{ not__yet: taskItem.tasks[0].taskId == null }">
+				<div class="check" v-if="taskItem.state == 0"><i class="iconfont el-icon-success iconbangzhu"/></div>
+				<div class="check" v-else-if="taskItem.state == 1"><i class="iconfont el-icon-success iconbangzhu"/></div>
+				<div class="check" v-else-if="taskItem.state == 2 || taskItem.state == 4 "><i class="iconfont el-icon-time iconbangzhu"/></div>
+				<div class="check" v-else-if="taskItem.state == 3"><i class="iconfont el-icon-more iconbangzhu need_grey"/></div>
+				
+				<!-- audit task wrapper -->
+				<div class="audit-task-wrapper" :class="{ on_audit : taskItem.state == 2 }" >
+					<div class="audit-workflow-name">{{taskItem.nodeName}}</div>
+					<div class="audit-flow-content"  v-for=" task in taskItem.tasks" :key="task.taskId">
+						<!-- name -->
+						<div class="for-flex justify-content_space-between" style="margin-bottom: 10px">
+							<div class="audit-name">
+								<div class="audit-user-name" v-if="task.assignee !== null && task.auditByUsers.length == 0 ">{{task.assignee.titleName}} -- {{task.assignee.userName}} <span>({{task.startTs}})</span></div>
+								<div class="audit-user-name" v-else-if="task.assignee == null && task.auditByUsers.length > 0"> {{task.auditByUsers[0].titleName}} -- {{task.auditByUsers[0].userName}} <span>({{taskItem.tasks[0].startTs}})</span></div>
 							</div>
-							<!-- description -->
-							<div class="audit-description">
-								<div class="audit-description-comment" v-if="task.comment !== null " >{{task.comment.description}}</div>
-								<div class="audit-description-data" v-if="task.comment !== null ">
-									<img :src="blopSign.content" alt="" v-for="blopSign in task.comment.signature" :key="blopSign.ts" style="background: #FFF" >
+							<div class="audit-situation"  v-if="taskItem.state == 1 || taskItem.state == 4">
+								<div class="audit_agree" v-if="task.comment.result == 0 && task.comment.result !== null"><i class="iconfont el-icon-check"/> 同意</div>
+								<div class="audit_disagree" v-else-if="task.comment.result == 1 && task.comment.result !== null"><i class="iconfont el-icon-close"/> 駁回</div>
+								<div class="audit_disagree" v-else-if="task.comment.result == -2"><i class="iconfont el-icon-info"/> 撤回</div>
+								<div class="audit_disagree" v-else-if="task.comment.result == -1"><i class="iconfont el-icon-info"/> 取消</div>
+							</div>
+						</div>
+						<!-- description -->
+						<div class="audit-description">
+							<div class="audit-description-comment" v-if="task.comment !== null " >{{task.comment.description}}</div>
+							<div class="audit-description-data" v-if="task.comment !== null ">
+								<img :src="blopSign.content" alt="" v-for="blopSign in task.comment.signature" :key="blopSign.ts" 
+								style="background: #FFF;" >
 
-									<div class="iimg"  v-for="blopImg in task.comment.attachment" :key="blopImg.ts">
-										<img :src="blopImg.url" alt="" v-if="blopImg.mediaType == 2">
-										<a class="pdfLink" :href="blopImg.url" v-if="blopImg.mediaType == 4"></a>
-									</div>
-									
+								<div class="iimg" v-for="blopImg in task.comment.attachment" :key="blopImg.ts">
+									<el-image
+										:src="blopImg.url"
+										v-if="blopImg.mediaType == 2"
+										:preview-src-list = "getImgSrc(index)"
+										/>
+									<a class="pdfLink" :href="blopImg.url" v-if="blopImg.mediaType == 4"></a>
 								</div>
+								
 							</div>
 						</div>
 					</div>
 				</div>
+			</div>
     </div>
 </template>
 <script>
@@ -55,13 +60,33 @@ export default {
 
 	data() {
 		return {
-		
+			imgListAry: [],
+			urlList: []
 		};
 	},
 
-  methods: {
-    
-  }
+	mounted() {
+	},
+	methods: {
+		getImgSrc(index){
+			console.log('index', index)
+			var arr = []
+			if(this.taskInfo[index].tasks.length > 1){
+				this.taskInfo[index].tasks.forEach( t => {
+					var rulResult = t.comment.attachment.map( i => i = i.url )
+					arr.push(rulResult[0])
+				});
+			} else if(this.taskInfo[index].tasks.length = 1){
+				this.taskInfo[index].tasks.forEach( t => {
+					var rulResult = t.comment.attachment.map( i => i = i.url )
+					arr = rulResult
+				});
+			}
+			return this.urlList = arr
+		},
+		
+		
+	}
 };
 </script>
 <style lang="sass" scoped>
@@ -83,6 +108,7 @@ export default {
 	.on_audit
 		border-top: 2px solid #006ab7
 		border-bottom: 2px solid #006ab7
+		padding-top: 10px !important
 
 	.for-flex
 		display: flex
@@ -112,10 +138,10 @@ export default {
 			padding-top: 10px
 			margin-bottom: 20px
 			.audit-workflow-name
-					font-size: 15px
-					font-weight: 900
-					margin-bottom: 3px
-					
+				font-size: 15px
+				font-weight: 900
+				margin-bottom: 3px
+				
 
 			.audit-flow-content
 				// padding-top: 0px !important
@@ -153,18 +179,33 @@ export default {
 					padding: 20px
 					.audit-description-comment
 						font-size: 14px
+						word-wrap: break-word
+
 					.audit-description-data
 						display: flex
 						flex-wrap: wrap
 						flex-direction: row
 						justify-content: flex-start
 						align-items: flex-start
+						.iimg
+							.el-image
+								height: 120px
+								margin-top: 10px
+								margin-right: 10px
+								border-radius: 4px
+								img
+									width: auto
+									height: 120px
+									border-radius: 4px
+									cursor: pointer !important
 						img
-							margin-top: 10px
-							margin-right: 10px
-							width: auto
-							height: 120px
-							border-radius: 4px
+								margin-top: 10px
+								margin-right: 10px
+								border-radius: 4px
+								width: auto
+								height: 120px
+								border-radius: 4px
+								cursor: pointer !important
 						.pdfLink
 							display: block
 							margin-top: 10px
@@ -180,5 +221,5 @@ export default {
 							background: url("~@/../static/img/PDF_file_icon.svg") center
 							background-size: contain
 							background-repeat: no-repeat
-							
+
 </style>
--- src/views/audit/details/AuditDetail.vue
+++ src/views/audit/details/AuditDetail.vue
@@ -1,37 +1,69 @@
 <template>
   <div v-loading="isLoadingData" class="setting-details self-loading">
-  <div class="page-container report-setting paper " style="height: 100%">
-    <div class="audit-section">
-      <!-- audit-header -->
-      <div class="audit-header">
-        <h3>簽核巡檢表</h3>
-        <div class="goto-report" 
-          @click="goToReportdetails"
-          v-if="showingBtn ">報告詳情</div>
-      </div>
-    
-      <div v-loading="isLoadingData" class="setting-details self-loading">
-        <!-- audit body -->
-        <div class="audit-flow-body">
+    <div class="page-container report-setting paper " style="height: 100%">
+      <div class="audit-section">
+        <!-- audit-header -->
+        <div class="audit-header">
+          <h3>{{auditDetail.reportName}}</h3>
+          <div class="goto-report" 
+            @click="goToReportdetails"
+            v-if="showingBtn ">報告詳情</div>
+        </div>
+      
+        <div v-loading="isLoadingData" class="setting-details self-loading">
+          <!-- audit body -->
+          <div class="audit-flow-body">
 
-          <div class="audit-flow-ownerhandling">
-            <p style="margin-bottom: 30px">簽核流程</p>
-            <div class="handling" v-if="!onEditing">
-              <div class="withdraw" @click="taskDrawback" v-if="auditDetail.submitter == currentUserInfo" >撤回</div>
-              <div class="l-l" v-if="taskInfo.cancelable && auditDetail.submitter == currentUserInfo"> | </div>
-              <div class="cancel" @click="CancelWorkflow " v-if="taskInfo.cancelable && auditDetail.submitter == currentUserInfo">取消</div>
+            <div class="audit-flow-ownerhandling">
+              <p style="margin-bottom: 30px">簽核流程</p>
+              <div class="handling" v-if="!onEditing && auditDetail.auditState < 4">
+                <div class="withdraw" @click="showDoalogTaskDrawback = true" v-if="auditDetail.submitter == currentUserInfo" >撤回</div>
+                <div class="l-l" v-if="auditDetail.cancelable && auditDetail.submitter == currentUserInfo"> | </div>
+                <div class="cancel" @click="showDoalogTaskCancel = true " v-if="auditDetail.cancelable && auditDetail.submitter == currentUserInfo">取消</div>
+              </div>
             </div>
-          </div>
-          
-          <!-- task -->
-          <AuditUnit 
-            :taskInfo = "taskInfo"
-            />
+            
+            <!-- task -->
+            <AuditUnit 
+              :taskInfo = "taskInfo"
+              />
 
+          </div>
         </div>
       </div>
-    </div>
-  </div> 
+    </div> 
+    <!-- popup -->
+    <dialog-pop
+      title="撤回簽核？"
+      :append-to-body="true"
+      :close-on-click-modal="false"
+      :show-close="false"
+      :visible="showDoalogTaskDrawback"
+      :isWarning="true"
+      @visibleChangeHandler="updateDeleteContentDialogFlag($event, 'showDoalogTaskDrawback')"
+      @cancelHandler="hideDeleteContentDialog('showDoalogTaskDrawback')"
+      @confirmHandler="confirmTaskDrawback()"
+    >
+      <div class="dialog-slot">
+        <div class="dialog-content">確認是否撤回此報告簽核？ </div>
+      </div>
+    </dialog-pop>
+
+    <dialog-pop
+      title="取消簽核？"
+      :append-to-body="true"
+      :close-on-click-modal="false"
+      :show-close="false"
+      :visible="showDoalogTaskCancel"
+      :isWarning="true"
+      @visibleChangeHandler="updateDeleteContentDialogFlag($event, 'showDoalogTaskCancel')"
+      @cancelHandler="hideDeleteContentDialog('showDoalogTaskCancel')"
+      @confirmHandler="cancelWorkflow()"
+    >
+      <div class="dialog-slot">
+        <div class="dialog-content">確認是否取消此報告簽核？ </div>
+      </div>
+    </dialog-pop>
   </div>
 </template>
 <script>
@@ -43,12 +75,14 @@ import {
 
 import DelayButton from '@/components/DelayButton';
 import AuditUnit from '@/components/AuditUnit';
+import DialogPop from '@/components/DialogPop';
 
 
 export default {
   name: 'WorkflowDetail',
   components: {
-    AuditUnit
+    AuditUnit,
+    DialogPop
   },
   data() {
     return {
@@ -62,6 +96,8 @@ export default {
       showingBtn: true,
 
       onEditing: false,
+      showDoalogTaskDrawback : false,
+      showDoalogTaskCancel : false
     }
   },
   
@@ -91,6 +127,15 @@ export default {
         );
     },
 
+    hideDeleteContentDialog(key) {
+      this[key] = false;
+    },
+
+    confirmTaskDrawback(){
+      console.log('Let me taskDrawback this task');
+      this.taskDrawback()
+    },
+
     async init(){
       await this.getWorkflowInfo()
       await this.getTaskInfo(this.auditDetail.inspectReportId)
@@ -117,6 +162,16 @@ export default {
 
         this.taskInfo = res.data
         console.log('this.taskInfo 2 ----->> ', this.taskInfo);
+
+        // 刪除撤回前的 task
+        var drawbackNum = this.taskInfo.findLastIndex(i => {
+          if(i.tasks[0].comment !== null){
+            return i.tasks[0].comment.description == "drawback"
+          }
+        })
+        var totalNum = this.taskInfo.length
+        this.taskInfo = this.taskInfo.slice(drawbackNum - totalNum + 1 )
+
         this.isLoadingData = false
       }).catch(err => {
         this.isLoadingData = false;
@@ -126,43 +181,44 @@ export default {
     
 
     taskDrawback(){
-      this.isLoadingData = true
       const value = {
         "inspectReportId" : this.auditDetail.inspectReportId
         }
       taskDrawback(value).then(res=>{
         this.isLoadingData = false
         var reportId = this.auditDetail.inspectReportId
-        this.$router.push(
-          { 
-            name: 'auditReportdetails', 
-            params: {
-              reportId: reportId, 
-              isAuditMode: true, 
-              canEdit: true,
-              canCancel: this.auditDetail.cancelable && (this.auditDetail.auditState==2 ||this.auditDetail.auditState==3 || this.auditDetail.auditState==6)
-              }
-          }
-        );
+        this.$router.push({name: "SendAuditManage"})
+        // this.$router.push(
+        //   { 
+        //     name: 'auditReportdetails', 
+        //     params: {
+        //       reportId: reportId, 
+        //       isAuditMode: true, 
+        //       canEdit: true,
+        //       canCancel: this.auditDetail.cancelable && (this.auditDetail.auditState==2 ||this.auditDetail.auditState==3 || this.auditDetail.auditState==6)
+        //       }
+        //   }
+        // );
       }).catch(err => {
         this.isLoadingData = false;
         console.log('error' + err);
       });
     },
 
-    CancelWorkflow(){
-      this.isLoadingData = true
+    cancelWorkflow(){
       const value = {
         "inspectReportId" : this.auditDetail.inspectReportId
         }
+      console.log('cencel')
       CancelWorkflow(value).then(res=>{
         this.isLoadingData = false
-        this.$router.push({name: "AuditManage"})
+        this.$router.push({name: "SendAuditManage"})
       }).catch(err => {
         this.isLoadingData = false;
         console.log('error' + err);
       });
     },
+
 
     // 判斷駁回取消按鈕顯示
     isEditing(){
