9ffba454be8c0ef3aba1df803fd0597541c9f817
Rainney <rainney.chen@advantech.com.tw>
2022-08-22 16:20:48 +0800

    Fix Bug #7609:[簽核管理] 變更每頁顯示筆數後切頁再切回，顯示筆數會超過10筆，與每頁顯示10筆的設定不符.

--- src/views/audit/TranscriptNotify.vue
+++ src/views/audit/TranscriptNotify.vue
@@ -262,7 +262,7 @@ export default{
                 this.inputSearchValue = searchParams.inputSearchValue;
                 //this.curState = searchParams.curState;
                 this.curStoreIds = searchParams.filterStoreIds;
-                this.curSizeNum = (typeof searchParams.sizeNum=='undefined')?10:searchParams.sizeNum;
+                //this.curSizeNum = (typeof searchParams.sizeNum=='undefined')?10:searchParams.sizeNum;
                 this.curOrder = (typeof searchParams.order=='undefined')?{direction:'desc',property:'processLastUpdateTs'}:searchParams.order;
                 this.defaultSort = {order:(this.curOrder.direction=='desc')?'descending':'ascending',prop:this.curOrder.property};
                 this.searchParams = searchParams;
