b9e95da994bc7c789d531777aa837ac5f922e37f
Rainney <rainney.chen@advantech.com.tw>
2022-10-07 10:28:41 +0800

    修正:當畫面縮小，太多附件，會跑出去的問題

--- src/views/patrolShop/ConfirmAddSum.vue
+++ src/views/patrolShop/ConfirmAddSum.vue
@@ -845,7 +845,7 @@ export default {
             }).catch((err) => {
               upload++;
             });
-            if (upload !== 0) {imgFileList
+            if (upload !== 0) {
               self.uploadProgress = false;
               util.notify(self.$t('remotePatrol.sentFail'), 'error', 3000);
               return false;
@@ -2032,6 +2032,7 @@ export default {
         }
         .attach-area{
           display:flex;
+          flex-wrap: wrap;
           align-content:flex-start;
           align-self: flex-start;
           .attach-add{
