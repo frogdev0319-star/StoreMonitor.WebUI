63384926e8a1083301798a1f978089b188d06bae
Rainney <rainney.chen@advantech.com.tw>
2022-11-04 10:35:07 +0800

    如果圖表中的值最大值高過100，會顯示該圖表最大值

--- src/views/statistical/PatrolCompareStat.vue
+++ src/views/statistical/PatrolCompareStat.vue
@@ -905,7 +905,7 @@ export default {
           },
           max:function (value) {
             if(!value || value.max==0 ||  value.max<=100) return 100;
-            else return value.max+10;
+            else return value.max;
           },
           axisLine:{
             show:false,
