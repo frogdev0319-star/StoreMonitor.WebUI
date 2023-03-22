1eb3306fa220ac2bda58c6e12ccf6295ef9e53b9
albert <Albert.Deng@advantech.com.tw>
2023-03-22 17:38:57 +0800

    v3.0.6.11

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -273,7 +273,7 @@
           <el-col :sapn="24" class="footercontent">
             <footer class="footerInfo">
               <p style="text-align: left">
-                v3.0.6.10
+                v3.0.6.11
                   &copy; {{ getFullYear }} Advantech Intelligent City
                   Services Co., Ltd. (AiCS) All Rights Reserved.
               </p>
