53ea42865eb8c24869f849e6bb468cc2f74907a4
Albert Deng <frogdeng0319@gmail.com>
2023-07-03 03:22:57 +0800

    v3.1.2.4

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -273,7 +273,7 @@
           <el-col :sapn="24" class="footercontent">
             <footer class="footerInfo">
               <p style="text-align: left">
-                v3.1.2.3
+                v3.1.2.4
                   &copy; {{ getFullYear }} Advantech Intelligent City
                   Services Co., Ltd. (AiCS) All Rights Reserved.
               </p>
@@ -602,7 +602,7 @@ export default {
     this.showMimicMode = this.$store.getters.ShowMimicMode;
     this.hasMystery = this.$store.getters.isMystery;
 
-    this.$store.dispatch("GetWhiteList");
+  
     
   },
 
