5b45d0df8a52643e46b3676a82572ab46ec56c6a
albert <Albert.Deng@advantech.com.tw>
2023-07-11 15:01:34 +0800

    getWhiteLsit

--- src/views/setting/title/TitleSetting.vue
+++ src/views/setting/title/TitleSetting.vue
@@ -450,19 +450,18 @@ export default {
       const whiteList = this.$store.getters.whiteList
       const accountId = this.$store.getters.accountId
 
-      console.log('whiteList~~', whiteList)
-      console.log('accountId ~~', accountId)
+      console.log('whiteList!!!!!!!!!!!!!!!!!!!!', whiteList)
+      console.log('accountId !!!!!!!!!!!!!!!!!!!!', accountId)
 
-      if(whiteList){
-        this.isShowing = whiteList.some( i => i == accountId)
-        console.log('this.isShowing titleSetting', this.isShowing)
+      this.isShowing = whiteList.some( i => i == accountId)
+      console.log('this.isShowing', this.isShowing)
 
-        if(this.isShowing){
-          this.roleNameList[5].show = true
-          } else {
-            this.roleNameList[5].show = false
-          }
-        }
+      if(this.isShowing){
+        this.roleNameList[5].show = true
+      } else {
+        this.roleNameList[5].show = false
+      }
+
     },
 
     getLangStyleValue(langArray){
