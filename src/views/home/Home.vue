f13365df437d29e70fd3691da5df4e70befd684f
Albert Deng <frogdeng0319@gmail.com>
2023-10-30 01:24:49 +0800

    v3.1.5.1

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -285,7 +285,7 @@
           <el-col :sapn="24" class="footercontent">
             <footer class="footerInfo">
               <p style="text-align: left">
-                v3.1.5.0
+                v3.1.5.1
                   &copy; {{ getFullYear }} Advantech Intelligent City
                   Services Co., Ltd. (AiCS) All Rights Reserved.
               </p>
