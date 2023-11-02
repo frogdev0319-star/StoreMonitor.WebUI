d61e41f80ccb3f62c418965e3a100d5b7b279601
Albert Deng <frogdeng0319@gmail.com>
2023-11-03 03:02:37 +0800

    fixed patrolEvaluation page

--- src/views/statistical/PatrolEvaluationStatistics.vue
+++ src/views/statistical/PatrolEvaluationStatistics.vue
@@ -3018,7 +3018,11 @@ export default {
 
         async bigScorefunc(val){
             console.log('aaa param :>> ', val);
-            const allStoreResult = await self.getInspectStatsOverviewWithGroup(oriParam);
+            const allStoreResult = await this.getInspectStatsOverviewWithGroup(val);
+            console.log('allStoreResult :>> ', allStoreResult);
+            let totalReport = 0;
+            let totalStandard = 0;
+
             if (allStoreResult.errCode === 0) {
                 const aaa = allStoreResult.data;
                 if (aaa) {
