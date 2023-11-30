d91ddc2e098a2e2b8e8b5ca97194a86bd6437ea3
albert <Albert.Deng@advantech.com.tw>
2023-11-30 15:39:57 +0800

    #11260 #11272 #11263 #11262 #11255

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -537,6 +537,7 @@ export default {
         { curPath: ["/scheduleDetailModify"], activePath: "/scheduleSetting" },
 
         { curPath: ["/waterMark"], activePath: "/waterMark" },
+        { curPath: ["/dataSecurity"], activePath: "/dataSecurity" },
         { curPath: ["/send"], activePath: "/send" },
         { curPath: ["/sendingRecord"], activePath: "/sendingRecord" },
 
