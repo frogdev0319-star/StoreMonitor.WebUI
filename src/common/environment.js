9e47f88acc39b8251cc680f7abeab5d33adc5224
albert <Albert.Deng@advantech.com.tw>
2025-11-19 17:26:10 +0800

    iservicePreview

--- src/common/environment.js
+++ src/common/environment.js
@@ -113,7 +113,7 @@ else if (data.SITE == 'iservice-preview') {
   isGlobalWebsite = true;
 }
 else {
-  environJSON = _Environments.RC_iService;
+  environJSON = _Environments.iservicePreview;
   isGlobalWebsite = true;
 }
 
